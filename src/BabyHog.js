6b60e9a6947cf6f95dd380fd5e19df5926eb4a95
Roysan Easo <easo.roysan@gmail.com>
2019-07-17 10:07:52 -0500

    Done.

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -10,8 +10,23 @@ import GlowingBaby from './assets/glowing-eyes.png'
 
 export default class BabyHog extends Component {
 
+  static defaultProps={
+    eyeColor: "normal"
+  }
+
   constructor(props) {
     super(props)
+
+    this.color = {
+      "blue": BlueBaby,
+      "sun": SunBaby,
+      "glowing": GlowingBaby,
+      "normal": normalBaby
+    }
+
+    this.state={
+      weight: 100
+    }
   }
 
   changeWeight = (e) => {
@@ -25,20 +40,20 @@ export default class BabyHog extends Component {
   render() {
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
+        <h1>Name: {this.props.name}</h1>
+        <h3>Weight: {this.state.weight}</h3>
+        <h3>Hobby: {this.props.hobby}</h3>
+        <h4>Eye Color: {this.props.eyeColor}</h4>
           
-        <Button name="+">
+        <Button name="+" onClick={this.changeWeight}>
           Increase Weight
         </Button>
-        <Button name="-">
+        <Button name="-" onClick={this.changeWeight}>
           Decrease Weight
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+          <img src={this.color[this.props.eyeColor]} style={{height: '200px'}} alt="MasterBlasterJrJr" />
         </div>
         
       </li>
